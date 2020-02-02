8c4880043750dacbd31fd88ffdcb6bdc2b114989
Kari <kariminger@gmail.com>
2020-02-01 22:03:37 -0600

    Add Organizm code to repo for deployment

---
+++ src/components/layout/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Tasks from "../tasks/Tasks";
+
+export default function Content() {
+    
+    return (
+        <div className="content">
+            <Tasks />
+        </div>
+    )
+}
