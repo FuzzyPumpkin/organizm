a063e998e10fefa199acc70904b1f43fcf2f8dca
Kari <kariminger@gmail.com>
2020-02-01 22:27:21 -0600

    Allow upload of build folder

---
+++ build/precache-manifest.c6a5bc438be2276c4bb333d98b43350a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd2ca3c8a55293953ed3aa6410bbe7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24436942946747d1d4d0",
+    "url": "/static/css/main.ad2a0ef3.chunk.css"
+  },
+  {
+    "revision": "0abe7c207607c5de0919",
+    "url": "/static/js/2.88a95acc.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/static/js/2.88a95acc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24436942946747d1d4d0",
+    "url": "/static/js/main.546f27e7.chunk.js"
+  },
+  {
+    "revision": "c18cd92b65bbedbeb51e",
+    "url": "/static/js/runtime-main.dcfaa9cc.js"
+  }
+]);
